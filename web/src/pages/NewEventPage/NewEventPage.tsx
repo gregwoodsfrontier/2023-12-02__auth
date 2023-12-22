8015c39fda8ccdff82d4c94da8c624609b9f860e
Richard Chung <iwannaplaygamesjp@gmail.com>
2023-12-22 03:43:39 +0000

    setting up the new event page

--- web/src/pages/NewEventPage/NewEventPage.tsx
+++ web/src/pages/NewEventPage/NewEventPage.tsx
@@ -2,13 +2,24 @@ import { DateField, Form, Label, TextField } from '@redwoodjs/forms'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import Checkbox from 'src/components/Checkbox/Checkbox'
+import HeaderWithRulers from 'src/components/HeaderWithRulers/HeaderWithRulers'
+
 const NewEventPage = () => {
+  const handleSubmit = (data) => {
+    console.log({ data })
+  }
+
   return (
     <>
-      <MetaTags title="Set up a new group" />
+      <MetaTags title="Set up your event" />
 
       <div className="auth-form">
-        <Form>
+        <HeaderWithRulers
+          heading="Set Up Your Event"
+          className="mb-8 text-white"
+        />
+        <Form onSubmit={handleSubmit}>
           <div className="field">
             <Label name="eventName">Group Name</Label>
             <TextField name="eventName" placeholder="" />
@@ -17,7 +28,13 @@ const NewEventPage = () => {
             <Label name="eventDate">Event Date</Label>
             <DateField name="eventDate" placeholder="" />
           </div>
-          <div className="field"></div>
+          <div className="field">
+            <Checkbox
+              name="eventReminder"
+              label="Send out a reminder for the event"
+            />
+          </div>
+          <button type="submit">Submit</button>
         </Form>
       </div>
     </>
